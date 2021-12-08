db889f52981f4533d5add095eb65473bba50a8dd
Qian Cao <63546190+qiancao@users.noreply.github.com>
2021-12-08 02:50:22 -0500

    day8

--- 2021day8/untitled.py
+++ 2021day8/untitled.py
@@ -20,9 +20,9 @@ with open("input") as file:
         outputList.append(ll[1].split())
         
 #%% Part I
+
 outputCharLengths = np.zeros((len(outputList),4))
-for ind in range(len(outputList)):
-    output = outputList[ind]
+for ind, output in enumerate(outputList):
     outputCharLengths[ind,:] = np.array([len(x) for x in output])
     
 total_number = 0
@@ -33,6 +33,9 @@ print(total_number)
 
 #%% Part II
 
+INVALID_PERMUTE = -1
+
+# Dictionary: String --> Displayed Number
 numbers = np.arange(10)
 displayCodes = ["abcefg","cf","acdeg","acdfg", # 0 1 2 3
                 "bdcf","abdfg","abdefg", # 4 5 6
@@ -40,6 +43,18 @@ displayCodes = ["abcefg","cf","acdeg","acdfg", # 0 1 2 3
 display2num = defaultdict(lambda: None)
 for ind in range(len(numbers)):
     display2num[frozenset(displayCodes[ind])] = str(numbers[ind])
+    
+def stringList2Number(stringList, codeDict):
+    numList = []
+    for ind, string in enumerate(stringList):
+        displayString = frozenset("".join([codeDict[char] for char in string]))
+        numList.append(display2num[displayString])
+        
+    if None not in numList:
+        number = int("".join(numList))
+        return number
+    else:
+        return INVALID_PERMUTE
 
 segments = "abcdefg"
 segmentsPermuted = list(permutations(segments))
@@ -47,28 +62,15 @@ segmentsPermuted = list(permutations(segments))
 total_sum = 0
 for ind, (signal, output) in enumerate(zip(signalList, outputList)):
     
-    number = -1
+    # Search all permutations for correct codeDict
     for segmentsTuple in segmentsPermuted:
-        
         segments2display = {segmentsTuple[i] : segments[i] for i in range(len(segmentsTuple))}
-        
-        numList = []
-        for ind, string in enumerate(signal):
-            displayString = frozenset("".join([segments2display[char] for char in string]))
-            numList.append(display2num[displayString])
-
-        if None not in numList:
-            number = int("".join(numList))
-            segments2displaySolution = segments2display
+        number = stringList2Number(signal, segments2display)
+        if number is not INVALID_PERMUTE:
             break
     
-    numList = []
-    for ind, string in enumerate(output):
-        displayString = frozenset("".join([segments2displaySolution[char] for char in string]))
-        numList.append(display2num[displayString])
-
-    if None not in numList:
-        number = int("".join(numList))
+    # Apply codeDict to output
+    number = stringList2Number(output, segments2display)
         
     total_sum = total_sum + number
     
