9d9e3c7144e54374256695872f94e56bbac0657f
Qian Cao <63546190+qiancao@users.noreply.github.com>
2021-12-24 22:27:09 -0500

    day 17

---
+++ 2021day18/solution_part1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: qcao
+"""
+
+import numpy as np
+
+e0 = list(np.loadtxt("test", dtype = str))
+e0 = np.array([[char for char in erow] for erow in e0]).astype(int)
+
+with open("test") as file:
+    for l, ll in enumerate(file):
+        ll = ll.rstrip()
+        ll = ll.split("|")
+
+        
+#%% Part I
+
+print()
+
